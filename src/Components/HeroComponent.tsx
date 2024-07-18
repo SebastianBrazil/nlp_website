fc218298911b748d8f6bf410098813e376e2126d
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-07-18 12:41:33 -0700

    i dont know whats breaking

--- src/Components/HeroComponent.tsx
+++ src/Components/HeroComponent.tsx
@@ -12,6 +12,7 @@ const HeroComponent = () => {
             fill={true}
             placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPs/w8AAiMBkMscdekAAAAASUVORK5CYII="
             priority={true}
+            style={{objectFit: "cover"}}
             className=""
             src={"/assets/heroPlaceholder.png"}
             alt="Hero Image"
