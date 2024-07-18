59ac22618cbfc545ad4db94ae9e2175632b69447
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-07-18 12:44:26 -0700

    show image pls

--- src/Components/HeroComponent.tsx
+++ src/Components/HeroComponent.tsx
@@ -7,7 +7,7 @@ const HeroComponent = () => {
   return (
     <>
       <section>
-        <div style={{position: "relative"}} className="w-screen h-[500px]">
+        <div style={{position: "relative"}} className="w-screen h-[500px] overflow-hidden">
           <Image
             fill={true}
             placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPs/w8AAiMBkMscdekAAAAASUVORK5CYII="
